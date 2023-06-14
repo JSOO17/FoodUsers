0bf8ce0d7f025eceec23a0ac821413a933e7cb39
JaiderOsorio <JaiderOsorio@DESKTOP-DC3SJGO>
2023-06-14 09:02:50 -0500

    init

--- FoodUsers.Infrastructure.API/Controllers/UserController.cs
+++ FoodUsers.Infrastructure.API/Controllers/UserController.cs
@@ -13,8 +13,6 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
 namespace FoodUsers.Infrastructure.API.Controllers
 {
     [Route("api/[controller]")]
